f7f7c922774a375298b3047a477a7b95cb66f312
set <set>
2026-02-11 19:30:39 +0100

    Shorten time-ago labels

--- src/main/lobjur/widgets/shared.cljs
+++ src/main/lobjur/widgets/shared.cljs
@@ -16,21 +16,27 @@
 ;;taken from https://stackoverflow.com/a/69122877/11189772
 (def time-ago (js* "function(input) {
   const date = (input instanceof Date) ? input : new Date(input);
-  const formatter = new Intl.RelativeTimeFormat('en');
   const ranges = {
-    years: 3600 * 24 * 365,
-    months: 3600 * 24 * 30,
-    weeks: 3600 * 24 * 7,
     days: 3600 * 24,
     hours: 3600,
     minutes: 60,
     seconds: 1
   };
+  const units = {
+    days: 'd',
+    hours: 'h',
+    minutes: 'm',
+    seconds: 's'
+  };
   const secondsElapsed = (date.getTime() - Date.now()) / 1000;
   for (let key in ranges) {
-    if (ranges[key] < Math.abs(secondsElapsed)) {
+    if ((key === 'days')
+        ? ranges[key] <= Math.abs(secondsElapsed)
+        : ranges[key] < Math.abs(secondsElapsed)) {
       const delta = secondsElapsed / ranges[key];
-      return formatter.format(Math.round(delta), key);
+      const rounded = Math.round(delta);
+      const label = Math.abs(rounded) + units[key];
+      return rounded < 0 ? label + ' ago' : 'in ' + label;
     }
   }
 }
