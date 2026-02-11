a1efcd95686c7a6d40ae16c43bb5d998d8f7f088
set <set>
2026-02-11 23:53:15 +0100

    Cleanup unused hn code

--- src/main/hackernews/core.cljs
+++ src/main/hackernews/core.cljs
@@ -50,29 +50,9 @@
   []
   (fetch-story-ids "beststories"))
 
-(defn ask-story-ids
-  "Get IDs of the latest Ask HN stories."
-  []
-  (fetch-story-ids "askstories"))
-
-(defn show-story-ids
-  "Get IDs of the latest Show HN stories."
-  []
-  (fetch-story-ids "showstories"))
-
-(defn job-story-ids
-  "Get IDs of the latest job stories."
-  []
-  (fetch-story-ids "jobstories"))
-
 ;; Higher-level functions that fetch full items
 
 (def page-size 30)
-
-(defn- fetch-items
-  "Fetch multiple items by their IDs in parallel."
-  [ids]
-  (js/Promise.all (mapv item ids)))
 
 (defn paginate-ids
   "Get a page of IDs from a list."
@@ -80,74 +60,6 @@
   (let [start (* (dec page) page-size)
         end (+ start page-size)]
     (subvec (vec ids) start (min end (count ids)))))
-
-(defn top-stories
-  "Get top stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (top-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-(defn new-stories
-  "Get newest stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (new-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-(defn best-stories
-  "Get best stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (best-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-(defn ask-stories
-  "Get Ask HN stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (ask-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-(defn show-stories
-  "Get Show HN stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (show-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-(defn job-stories
-  "Get job stories, paginated."
-  [& {:keys [page] :or {page 1}}]
-  (-> (job-story-ids)
-      (.then #(paginate-ids % page))
-      (.then fetch-items)))
-
-;; Comment tree fetching
-
-(defn- fetch-comment-tree
-  "Recursively fetch a comment and its children."
-  [comment-id]
-  (-> (item comment-id)
-      (.then (fn [comment]
-               (if (or (nil? comment) (:deleted comment) (:dead comment))
-                 nil
-                 (if (seq (:kids comment))
-                   (-> (js/Promise.all (mapv fetch-comment-tree (:kids comment)))
-                       (.then (fn [children]
-                                (assoc comment :children (filterv some? children)))))
-                   (assoc comment :children [])))))))
-
-(defn item-with-comments
-  "Fetch an item with its full comment tree."
-  [item-id]
-  (-> (item item-id)
-      (.then (fn [story]
-               (if (seq (:kids story))
-                 (-> (js/Promise.all (mapv fetch-comment-tree (:kids story)))
-                     (.then (fn [children]
-                              (assoc story :children (filterv some? children)))))
-                 (assoc story :children []))))))
 
 ;; Search via Algolia
 
@@ -158,16 +70,3 @@
    (http/get "https://hn.algolia.com/api/v1/search"
              {:params {:query query :page page :hitsPerPage 30 :tags "story"}})
    parse-json))
-
-;; User submissions
-
-(defn user-stories
-  "Get stories submitted by a user, paginated."
-  [username & {:keys [page] :or {page 1}}]
-  (-> (user username)
-      (.then (fn [u]
-               (if-let [submitted (:submitted u)]
-                 (-> (fetch-items (paginate-ids submitted page))
-                     (.then (fn [items]
-                              (filterv #(= (:type %) "story") items))))
-                 [])))))
